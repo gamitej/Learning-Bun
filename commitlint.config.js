4368663e486d52a49af1a358e978937b448fe598
gamitej <gamitejpratapsingh998@gmail.com>
2026-03-29 19:59:14 +0530

    feat(initial): husky setup completed

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,22 @@
 export default {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'chore',
+        'refactor',
+        'docs',
+        'style',
+        'perf',
+        'test',
+        'ci',
+        'revert',
+        'build'
+      ],
+    ],
+  },
 };
